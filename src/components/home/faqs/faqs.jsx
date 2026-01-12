f63acef58d2e9b3cd9d423d4dbd75bb76a1ed7a0
YYH <nobodybutyour@foxmail.com>
2026-01-12 16:54:51 +0800

    临时提交

--- src/components/home/faqs/faqs.jsx
+++ src/components/home/faqs/faqs.jsx
@@ -58,59 +58,5 @@ const SkillsList = () => {
     setOpenItem(openItem === item ? null : item);
   };
 
-  return (
-    <div className="flex flex-col items-center px-4 text-left">
-      <div className="mx-auto w-full max-w-4xl">
-        <h2 className="text-white text-center text-4xl font-bold drop-shadow-[2px_2px_0_#7836cf]">
-          Sobre mí y mi trabajo
-        </h2>
-        <ul className="mt-8 space-y-4 text-lg drop-shadow-[2px_2px_0_#7836cf]">
-          {Object.entries(skills).map(([category, items]) => {
-            console.log("Category:", CategoryIcons[category]);  // 添加这行来查看每个category的值
-            return (
-            <li key={category} className="w-full">
-              <div
-                onClick={() => toggleItem(category)}
-                className="bg-gray-900 hover:bg-opacity-80 w-full cursor-pointer overflow-hidden rounded-2xl text-left transition-all"
-              >
-                <div className="flex items-center gap-3 p-4">
-                  {CategoryIcons[category]}
-                  <div className="flex grow items-center justify-between gap-2">
-                    <div className="max-w-[200px] min-w-0 overflow-hidden md:max-w-none">
-                      <span className="block truncate text-lg text-white drop-shadow-[1px_1px_0_#7836cf] font-bold">
-                        {category}
-                      </span>
-                    </div>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      className={`h-6 w-6 shrink-0 transform text-[#6a2cbb] transition-transform ${
-                        openItem === category ? "rotate-180" : ""
-                      }`}
-                    >
-                      <path d="M11.9999 13.1714L16.9497 8.22168L18.3639 9.63589L11.9999 15.9999L5.63599 9.63589L7.0502 8.22168L11.9999 13.1714Z"></path>
-                    </svg>
-                  </div>
-                </div>
-
-                <div
-                  className={`px-4 transition-all duration-300 ${
-                    openItem === category
-                      ? "max-h-[500px] pb-4 opacity-100"
-                      : "max-h-0 opacity-0"
-                  }`}
-                >
-                  <p className="text-[0.8em] text-white text-semibold ">
-                    {skills[category]}
-                  </p>
-                </div>
-              </div>
-            </li>
-            )})}
-        </ul>
-      </div>
-    </div>
-  );
 };
 export default SkillsList;
