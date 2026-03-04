cd3ab4e6d4c6edae757a421e90f25d4b3c5e9409
YYH <nobodybutyour@foxmail.com>
2026-03-04 11:58:48 +0800

    优化SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.toolkit.ren",
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
     server: {
