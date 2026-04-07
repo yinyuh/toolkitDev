607a5c33a6b371a69ed521cb15fd6be402055859
YYH <ojbk@gmail.com>
2026-04-08 00:32:40 +0800

    优化“在线 Cron 表达式生成器”

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.toolkit.ren",
+  build: {
+    inlineStylesheets: "always",
+  },
   integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
